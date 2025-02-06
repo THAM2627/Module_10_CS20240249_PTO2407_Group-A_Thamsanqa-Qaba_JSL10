d9cecea89e1e227b2eb2df5d177b25929888dea2
Thamsanqa Qaba <thamie.qaba@gmail.com>
2025-02-06 21:03:29 +0200

    array update

--- escapeVanillaJS.js
+++ escapeVanillaJS.js
@@ -11,7 +11,7 @@ document.addEventListener("DOMContentLoaded", () => {
     });
 
     document.getElementById("solveRoom2").addEventListener("click", () => {
-        const jsConcepts = new Set(['closure', 'scope', 'hoisting', 'IIFE', 'async/await', 'promises', 'callbacks']);
+        const jsConcepts = new Set(['closure', 'scope', 'hoisting', 'IIFE', 'async', 'promises', 'callbacks', 'await']);
         // 🪲 Bug: What's mssing from JS concepts?
         const reactConcepts = new Set(['components', 'jsx', 'hooks', 'async']);
         // 🪲 Bug: Incorrect function call
