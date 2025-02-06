b37b83d8a24f85523d50bfb4c87e45a1a459fc2d
Thamsanqa Qaba <thamie.qaba@gmail.com>
2025-02-06 20:58:17 +0200

    instersection logic update

--- escapeVanillaJS.js
+++ escapeVanillaJS.js
@@ -40,7 +40,7 @@ function findMostRecentBook(books) {
 }
 function findIntersection(setA, setB) {
     // 🪲 Bug: Incorrect logic
-    const intersection = new Set();
+    const intersection = new setA();
     for (const element of setA) {
         if (setB.has(element)) {
             intersection.add(element);
